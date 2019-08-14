f3c1a3ba93daf6691df4b84569425327418faba6
Tracie T <traciecodes@gmail.com>
2019-08-14 15:18:25 -0500

    2nd: Initial setup of store & provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {provider} from 'react-redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+function reducer() {
+    return {
+        title: 'This is in the Redux Store'
+    }
+}
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+rootElement
+);
